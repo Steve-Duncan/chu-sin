3837dbc320da50c77a23e0438785dd49ec089a68
Steve Duncan <Steve-Duncan@users.noreply.github.com>
2024-10-31 19:21:15 -0700

    Removed function to prevent spin down on render.com

--- app.js
+++ app.js
@@ -53,21 +53,4 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-// ////////////////////////////////////////////////////////////////////////
-// To keep site on onrender from spinning down due to inactivity
-const url = `https://chu-sin.onrender.com/`; // Replace with your Render URL
-const interval = 30000; // Interval in milliseconds (30 seconds)
-
-function reloadWebsite() {
-  axios.get(url)
-    .then(response => {
-      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
-    })
-    .catch(error => {
-      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
-    });
-}
-setInterval(reloadWebsite, interval);
-// ////////////////////////////////////////////////////////////////////////
-
 module.exports = app;
