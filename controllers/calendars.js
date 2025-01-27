bdaf6dfaaa374371ae829bfc4de79c630b6e0c85
Steve Duncan <Steve-Duncan@users.noreply.github.com>
2025-01-27 10:54:26 -0800

    Update calendars for March; add links for maps; remove News from Main.

--- controllers/calendars.js
+++ controllers/calendars.js
@@ -5,61 +5,73 @@
         // data for Kyoto Kita
         
         {
-            year: "2025",month: "2",cal_1: "cal-木",cal_2: "cal-2", arrDates: [13,27], note: ""
+            year: "2025",month: "2",cal_1: "cal-木",cal_2: "cal-1", arrDates: [13,27], note: ""
         },
         {
-            year: "2025",month: "1",cal_1: "cal-木",cal_2: "cal-1", arrDates: [9,23], note: ""
+            year: "2025",month: "3",cal_1: "cal-木",cal_2: "cal-2", arrDates: [13,27], note: ""
         },
         {
-            year: "2025",month: "2",cal_1: "cal-金",cal_2: "cal-2", arrDates: [7,21], note: "＊両日ともウィングス京都"
+            year: "2025",month: "2",cal_1: "cal-金",cal_2: "cal-1", arrDates: [7,21], note: "＊両日ともウィングス京都"
         },
         {
-            year: "2025",month: "1",cal_1: "cal-金",cal_2: "cal-1", arrDates: [17], note: ""
+            year: "2025",month: "3",cal_1: "cal-金",cal_2: "cal-2", arrDates: [7,21], note: "＊7日ウィングス京都"
         },
         {
-            year: "2025",month: "2",cal_1: "cal-土",cal_2: "cal-2", arrDates: [8,22], note: "＊22日ウィングス京都"
+            year: "2025",month: "2",cal_1: "cal-土",cal_2: "cal-1", arrDates: [8,22], note: "＊22日ウィングス京都"
         },
         {
-            year: "2025",month: "1",cal_1: "cal-土",cal_2: "cal-1", arrDates: [11,25], note: "※両日ともウィングス京都"
+            year: "2025",month: "3",cal_1: "cal-土",cal_2: "cal-2", arrDates: [8,22], note: "＊8日ウィングス京都"
         },
+        
+        
         // data for Kyoto Asahi
         {
-            year: "2025",month: "2",cal_1: "cal-3土",cal_2: "cal-2", arrDates: [15], note: ""
+            year: "2025",month: "2",cal_1: "cal-3土",cal_2: "cal-1", arrDates: [15], note: ""
         },
         {
-            year: "2025",month: "1",cal_1: "cal-3土",cal_2: "cal-1", arrDates: [18], note: ""
+            year: "2025",month: "3",cal_1: "cal-3土",cal_2: "cal-2", arrDates: [18], note: ""
         },
+        
+        
         // data for Kyoto JEUGIA
         {
-            year: "2025",month: "2",cal_1: "cal-2金",cal_2: "cal-2", arrDates: [28], note: ""
+            year: "2025",month: "2",cal_1: "cal-2金",cal_2: "cal-1", arrDates: [28], note: ""
         },
         {
-            year: "2025",month: "1",cal_1: "cal-2金",cal_2: "cal-1", arrDates: [24], note: ""
+            year: "2025",month: "3",cal_1: "cal-2金",cal_2: "cal-2", arrDates: [15], note: ""
         },
+        
+        
         // data for Fukui Wednesay month classes
         {
-            year: "2025",month: "2",cal_1: "cal-水",cal_2: "cal-2", arrDates: [5,19], note: "＊両日とも昼の部のみ"
+            year: "2025",month: "2",cal_1: "cal-水",cal_2: "cal-1", arrDates: [5,19], note: "＊両日とも昼の部のみ"
         },
         {
-            year: "2025",month: "1",cal_1: "cal-水",cal_2: "cal-1", arrDates: [8,22], note: "＊両日とも昼の部のみ"
+            year: "2025",month: "3",cal_1: "cal-水",cal_2: "cal-2", arrDates: [5,19], note: "＊両日とも昼のみ"
         },
+        
+        
         // data for Fukui Newspaper classes
         {
-            year: "2025",month: "2",cal_1: "cal-火",cal_2: "cal-2", arrDates: [4,18], note: ""
+            year: "2025",month: "2",cal_1: "cal-火",cal_2: "cal-1", arrDates: [4,18], note: ""
         },
         {
-            year: "2025",month: "1",cal_1: "cal-火",cal_2: "cal-1", arrDates: [7,21], note: ""
+            year: "2025",month: "3",cal_1: "cal-火",cal_2: "cal-2", arrDates: [4,18], note: ""
         },
+        
+        
         // data for Fukui Sabae classes
         {
-            year: "2025",month: "2",cal_1: "cal-月",cal_2: "cal-2", arrDates: [3,17], note: "＊両日とも昼の部のみ"
+            year: "2025",month: "2",cal_1: "cal-月",cal_2: "cal-1", arrDates: [3,17], note: "＊両日とも昼のみ"
         },
         {
-            year: "2025",month: "1",cal_1: "cal-月",cal_2: "cal-1", arrDates: [6,20], note: "＊両日とも昼の部のみ"
+            year: "2025",month: "3",cal_1: "cal-月",cal_2: "cal-2", arrDates: [3,17], note: "＊両日とも昼の部のみ"
         },
-
+    
     ];
 
 module.exports = {
     calendars
 };
+
+
