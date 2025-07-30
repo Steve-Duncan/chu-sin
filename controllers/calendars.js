9f8617a8d47d4bff3cdd8b887c6ede3badee905f
Steve Duncan <Steve-Duncan@users.noreply.github.com>
2025-07-29 18:58:07 -0700

    Update calendars for September. Change time for Fukui Shimbun. Remove Wed night class.

--- controllers/calendars.js
+++ controllers/calendars.js
@@ -5,71 +5,80 @@
         // data for Kyoto Kita
         
         {
-            year: "2025",month: "7",cal_1: "cal-木",cal_2: "cal-1", arrDates: [10,24], note: ""
+            year: "2025",month: "8",cal_1: "cal-木",cal_2: "cal-1", arrDates: [7,28], note: ""
         },
-        {
-            year: "2025",month: "8",cal_1: "cal-木",cal_2: "cal-2", arrDates: [7,28], note: ""
+                {
+            year: "2025",month: "9",cal_1: "cal-木",cal_2: "cal-2", arrDates: [11,25], note: ""
         },
 
+
         {
-            year: "2025",month: "7",cal_1: "cal-金",cal_2: "cal-1", arrDates: [18], note: "＊18日ウィングス京都"
+            year: "2025",month: "8",cal_1: "cal-金",cal_2: "cal-1", arrDates: [1,22], note: "＊1日ウィングス京都"
         },
-        {
-            year: "2025",month: "8",cal_1: "cal-金",cal_2: "cal-2", arrDates: [1,22], note: "＊1日ウィングス京都"
+                {
+            year: "2025",month: "9",cal_1: "cal-金",cal_2: "cal-2", arrDates: [5,19], note: "＊19日ウィングス京都"
         },
+
         
         {
-            year: "2025",month: "7",cal_1: "cal-土",cal_2: "cal-1", arrDates: [12,26], note: "＊12日ウィングス京都"
+            year: "2025",month: "8",cal_1: "cal-土",cal_2: "cal-1", arrDates: [9,23], note: ""
         },
-        {
-            year: "2025",month: "8",cal_1: "cal-土",cal_2: "cal-2", arrDates: [9,23], note: ""
+                {
+            year: "2025",month: "9",cal_1: "cal-土",cal_2: "cal-2", arrDates: [6,27], note: "＊27日ウィングス京都"
         },
+
+
 
         
         // data for Kyoto Asahi
         {
-            year: "2025",month: "7",cal_1: "cal-3土",cal_2: "cal-1", arrDates: [19], note: ""
+            year: "2025",month: "8",cal_1: "cal-3土",cal_2: "cal-1", arrDates: [30], note: ""
         },
-        {
-            year: "2025",month: "8",cal_1: "cal-3土",cal_2: "cal-2", arrDates: [30], note: ""
+                {
+            year: "2025",month: "9",cal_1: "cal-3土",cal_2: "cal-2", arrDates: [20], note: ""
         },
+
 
         
         // data for Kyoto JEUGIA
         {
-            year: "2025",month: "7",cal_1: "cal-2金",cal_2: "cal-1", arrDates: [25], note: ""
+            year: "2025",month: "8",cal_1: "cal-2金",cal_2: "cal-1", arrDates: [22], note: ""
         },
-        {
-            year: "2025",month: "8",cal_1: "cal-2金",cal_2: "cal-2", arrDates: [22], note: ""
+                {
+            year: "2025",month: "9",cal_1: "cal-2金",cal_2: "cal-2", arrDates: [26], note: ""
         },
+
 
         
         
         // data for Fukui Wednesday month classes
         {
-            year: "2025",month: "7",cal_1: "cal-水",cal_2: "cal-1", arrDates: [2,16], note: "＊両日とも昼のみ"
+            year: "2025",month: "8",cal_1: "cal-水",cal_2: "cal-1", arrDates: [6,20], note: "＊両日とも昼のみ"
         },
-        {
-            year: "2025",month: "8",cal_1: "cal-水",cal_2: "cal-2", arrDates: [6,20], note: "＊両日とも昼のみ"
+                {
+            year: "2025",month: "9",cal_1: "cal-水",cal_2: "cal-2", arrDates: [3,17], note: "＊両日とも昼のみ"
         },
+
 
         
         // data for Fukui Newspaper classes
         {
-            year: "2025",month: "7",cal_1: "cal-火",cal_2: "cal-1", arrDates: [1,15], note: ""
+            year: "2025",month: "8",cal_1: "cal-火",cal_2: "cal-1", arrDates: [5,19], note: ""
         },
-        {
-            year: "2025",month: "8",cal_1: "cal-火",cal_2: "cal-2", arrDates: [5,19], note: ""
+                {
+            year: "2025",month: "9",cal_1: "cal-火",cal_2: "cal-2", arrDates: [2,16], note: ""
         },
+
 
         
         // data for Fukui Sabae classes
         {
-            year: "2025",month: "7",cal_1: "cal-月",cal_2: "cal-1", arrDates: [14], note: ""
+            year: "2025",month: "8",cal_1: "cal-月",cal_2: "cal-1", arrDates: [4,18], note: ""
         },
-        {
-            year: "2025",month: "8",cal_1: "cal-月",cal_2: "cal-2", arrDates: [4,18], note: ""
+                {
+            year: "2025",month: "9",cal_1: "cal-月",cal_2: "cal-2", arrDates: [1], note: ""
         },
+
     
     ];
 
