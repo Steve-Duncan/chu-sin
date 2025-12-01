e1d8707d947b1ce1e0993debc1de734ecdd051d3
Steve Duncan <Steve-Duncan@users.noreply.github.com>
2025-11-30 17:03:03 -0800

    Update calendar for January

--- controllers/calendars.js
+++ controllers/calendars.js
@@ -5,72 +5,78 @@
         // data for Kyoto Kita
         
         {
-            year: "2025",month: "11",cal_1: "cal-木",cal_2: "cal-1", arrDates: [13,27], note: ""
+            year: "2025",month: "12",cal_1: "cal-木",cal_2: "cal-1", arrDates: [11,25], note: ""
         },
-        {
-            year: "2025",month: "12",cal_1: "cal-木",cal_2: "cal-2", arrDates: [11,25], note: "＊25日 昼/夜ともウィングス京都"
+                {
+            year: "2026",month: "1",cal_1: "cal-木",cal_2: "cal-2", arrDates: [15,29], note: "＊25日 昼/夜ともウィングス京都"
         },
 
         {
-            year: "2025",month: "11",cal_1: "cal-金",cal_2: "cal-1", arrDates: [7,21], note: "＊21日ウィングス京都"
+            year: "2025",month: "12",cal_1: "cal-金",cal_2: "cal-1", arrDates: [5,19], note: "＊両日ウィングス京都"
         },
         {
-            year: "2025",month: "12",cal_1: "cal-金",cal_2: "cal-2", arrDates: [5,19], note: "＊両日ウィングス京都"
+            year: "2026",month: "1",cal_1: "cal-金",cal_2: "cal-2", arrDates: [16], note: "＊ウィングス京都"
         },
 
         
         {
-            year: "2025",month: "11",cal_1: "cal-土",cal_2: "cal-1", arrDates: [8,22], note: "＊両日ともウィングス京都"
+            year: "2025",month: "12",cal_1: "cal-土",cal_2: "cal-1", arrDates: [13], note: ""
         },
         {
-            year: "2025",month: "12",cal_1: "cal-土",cal_2: "cal-2", arrDates: [13], note: ""
+            year: "2026",month: "1",cal_1: "cal-土",cal_2: "cal-2", arrDates: [10,24], note: "＊両日ともウィングス京都"
         },
+
 
 
         // data for Kyoto Asahi
         // now this is Gocomachi 
         {
-            year: "2025",month: "11",cal_1: "cal-3土",cal_2: "cal-1", arrDates: [15], note: ""
+            year: "2025",month: "12",cal_1: "cal-3土",cal_2: "cal-1", arrDates: [20], note: ""
         },
         {
-            year: "2025",month: "12",cal_1: "cal-3土",cal_2: "cal-2", arrDates: [20], note: ""
+            year: "2026",month: "1",cal_1: "cal-3土",cal_2: "cal-2", arrDates: [17], note: ""
         },
+
 
 
         // data for Kyoto JEUGIA
         {
-            year: "2025",month: "11",cal_1: "cal-2金",cal_2: "cal-1", arrDates: [28], note: ""
+            year: "2025",month: "12",cal_1: "cal-2金",cal_2: "cal-1", arrDates: [26], note: ""
         },
         {
-            year: "2025",month: "12",cal_1: "cal-2金",cal_2: "cal-2", arrDates: [26], note: ""
+            year: "2026",month: "1",cal_1: "cal-2金",cal_2: "cal-2", arrDates: [23], note: ""
         },
+
 
         
         // data for Fukui Wednesday month classes
         {
-            year: "2025",month: "11",cal_1: "cal-水",cal_2: "cal-1", arrDates: [5,19], note: ""
+            year: "2025",month: "12",cal_1: "cal-水",cal_2: "cal-1", arrDates: [3,17], note: ""
         },
         {
-            year: "2025",month: "12",cal_1: "cal-水",cal_2: "cal-2", arrDates: [3,17], note: ""
+            year: "2026",month: "1",cal_1: "cal-水",cal_2: "cal-2", arrDates: [21], note: ""
         },
+
 
 
         // data for Fukui Newspaper classes
         {
-            year: "2025",month: "11",cal_1: "cal-火",cal_2: "cal-1", arrDates: [4,18], note: ""
+            year: "2025",month: "12",cal_1: "cal-火",cal_2: "cal-1", arrDates: [2,16], note: ""
         },
         {
-            year: "2025",month: "12",cal_1: "cal-火",cal_2: "cal-2", arrDates: [2,16], note: ""
+            year: "2026",month: "1",cal_1: "cal-火",cal_2: "cal-2", arrDates: [20], note: ""
         },
+
 
 
         // data for Fukui Sabae classes
         {
-            year: "2025",month: "11",cal_1: "cal-月",cal_2: "cal-1", arrDates: [17], note: ""
+            year: "2025",month: "12",cal_1: "cal-月",cal_2: "cal-1", arrDates: [1,15], note: ""
         },
         {
-            year: "2025",month: "12",cal_1: "cal-月",cal_2: "cal-2", arrDates: [1,15], note: ""
+            year: "2026",month: "1",cal_1: "cal-月",cal_2: "cal-2", arrDates: [19], note: ""
         },
+
     ];
 
 module.exports = {
