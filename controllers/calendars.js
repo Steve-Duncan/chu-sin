969503b0c24b13ba078150a01af5cc4d9cebaca7
Steve Duncan <Steve-Duncan@users.noreply.github.com>
2026-01-28 08:38:54 -0800

    Update calendar for March.

--- controllers/calendars.js
+++ controllers/calendars.js
@@ -5,28 +5,28 @@
         // data for Kyoto Kita
         
         {
-            year: "2026",month: "1",cal_1: "cal-木",cal_2: "cal-1", arrDates: [15,29], note: ""
+            year: "2026",month: "2",cal_1: "cal-木",cal_2: "cal-1", arrDates: [12,26], note: "＊26日午後のみウィングス京都"
         },
-                {
-            year: "2026",month: "2",cal_1: "cal-木",cal_2: "cal-2", arrDates: [12,26], note: "＊26日午後のみウィングス京都"
+        {
+            year: "2026",month: "3",cal_1: "cal-木",cal_2: "cal-2", arrDates: [12,26], note: ""
         },
 
 
         {
-            year: "2026",month: "1",cal_1: "cal-金",cal_2: "cal-1", arrDates: [16], note: "＊ウィングス京都"
+            year: "2026",month: "2",cal_1: "cal-金",cal_2: "cal-1", arrDates: [6,20], note: "＊20日ウィングス京都"
         },
-                {
-            year: "2026",month: "2",cal_1: "cal-金",cal_2: "cal-2", arrDates: [6,20], note: "＊20日ウィングス京都"
+        {
+            year: "2026",month: "3",cal_1: "cal-金",cal_2: "cal-2", arrDates: [6], note: "＊ウィングス京都"
         },
-
 
         
         {
-            year: "2026",month: "1",cal_1: "cal-土",cal_2: "cal-1", arrDates: [10,24], note: "＊両日ともウィングス京都"
+            year: "2026",month: "2",cal_1: "cal-土",cal_2: "cal-1", arrDates: [14,28], note: "＊両日ともウィングス京都"
         },
-                {
-            year: "2026",month: "2",cal_1: "cal-土",cal_2: "cal-2", arrDates: [14,28], note: "＊両日ともウィングス京都"
+        {
+            year: "2026",month: "3",cal_1: "cal-土",cal_2: "cal-2", arrDates: [14,28], note: "＊両日ともウィングス京都"
         },
+
 
 
 
@@ -34,43 +34,38 @@
         // data for Kyoto Asahi
         // now this is Gocomachi 
         {
-            year: "2026",month: "1",cal_1: "cal-3土",cal_2: "cal-1", arrDates: [17], note: ""
+            year: "2026",month: "2",cal_1: "cal-3土",cal_2: "cal-1", arrDates: [21], note: ""
         },
-                {
-            year: "2026",month: "2",cal_1: "cal-3土",cal_2: "cal-2", arrDates: [21], note: ""
+        {
+            year: "2026",month: "3",cal_1: "cal-3土",cal_2: "cal-2", arrDates: [21], note: ""
         },
-
-
 
 
         // data for Kyoto JEUGIA
         {
-            year: "2026",month: "1",cal_1: "cal-2金",cal_2: "cal-1", arrDates: [23], note: ""
+            year: "2026",month: "2",cal_1: "cal-2金",cal_2: "cal-1", arrDates: [27], note: ""
         },
         {
-            year: "2026",month: "2",cal_1: "cal-2金",cal_2: "cal-2", arrDates: [27], note: ""
+            year: "2026",month: "3",cal_1: "cal-2金",cal_2: "cal-2", arrDates: [27], note: ""
         },
-
 
 
         
         // data for Fukui Wednesday month classes
         {
-            year: "2026",month: "1",cal_1: "cal-水",cal_2: "cal-1", arrDates: [21], note: ""
+            year: "2026",month: "2",cal_1: "cal-水",cal_2: "cal-1", arrDates: [18], note: ""
         },
         {
-            year: "2026",month: "2",cal_1: "cal-水",cal_2: "cal-2", arrDates: [18], note: ""
+            year: "2026",month: "3",cal_1: "cal-水",cal_2: "cal-2", arrDates: [4,18], note: ""
         },
-
-
 
 
         // data for Fukui Newspaper classes
         {
-            year: "2026",month: "1",cal_1: "cal-火",cal_2: "cal-1", arrDates: [20], note: ""
+            year: "2026",month: "2",cal_1: "cal-火",cal_2: "cal-1", arrDates: [17], note: ""
         },
         {
-            year: "2026",month: "2",cal_1: "cal-火",cal_2: "cal-2", arrDates: [17], note: ""
+            year: "2026",month: "3",cal_1: "cal-火",cal_2: "cal-2", arrDates: [3,17], note: ""
         },
 
 
@@ -78,10 +73,10 @@
 
         // data for Fukui Sabae classes
         {
-            year: "2026",month: "1",cal_1: "cal-月",cal_2: "cal-1", arrDates: [19], note: ""
+            year: "2026",month: "2",cal_1: "cal-月",cal_2: "cal-1", arrDates: [16], note: ""
         },
-        {
-            year: "2026",month: "2",cal_1: "cal-月",cal_2: "cal-2", arrDates: [16], note: ""
+                {
+            year: "2026",month: "3",cal_1: "cal-月",cal_2: "cal-2", arrDates: [2,16], note: ""
         },
 
 
