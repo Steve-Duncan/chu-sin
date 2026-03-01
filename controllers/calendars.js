712137204a25740dc543a4c15a0e81bb826bd986
Steve Duncan <Steve-Duncan@users.noreply.github.com>
2026-02-28 18:02:03 -0800

    Update calendars for April

--- controllers/calendars.js
+++ controllers/calendars.js
@@ -5,67 +5,66 @@
         // data for Kyoto Kita
         
         {
-            year: "2026",month: "2",cal_1: "cal-木",cal_2: "cal-1", arrDates: [12,26], note: "＊26日午後のみウィングス京都"
+            year: "2026",month: "3",cal_1: "cal-木",cal_2: "cal-1", arrDates: [12,26], note: ""
         },
         {
-            year: "2026",month: "3",cal_1: "cal-木",cal_2: "cal-2", arrDates: [12,26], note: ""
+            year: "2026",month: "4",cal_1: "cal-木",cal_2: "cal-2", arrDates: [9,23], note: ""
         },
 
 
         {
-            year: "2026",month: "2",cal_1: "cal-金",cal_2: "cal-1", arrDates: [6,20], note: "＊20日ウィングス京都"
+            year: "2026",month: "3",cal_1: "cal-金",cal_2: "cal-1", arrDates: [6], note: "＊ウィングス京都"
         },
         {
-            year: "2026",month: "3",cal_1: "cal-金",cal_2: "cal-2", arrDates: [6], note: "＊ウィングス京都"
+            year: "2026",month: "4",cal_1: "cal-金",cal_2: "cal-2", arrDates: [3,17], note: "＊3日ウィングス京都／17日オフィスゴコマチ"
         },
 
         
         {
-            year: "2026",month: "2",cal_1: "cal-土",cal_2: "cal-1", arrDates: [14,28], note: "＊両日ともウィングス京都"
+            year: "2026",month: "3",cal_1: "cal-土",cal_2: "cal-1", arrDates: [14,28], note: ""
         },
-        {
-            year: "2026",month: "3",cal_1: "cal-土",cal_2: "cal-2", arrDates: [14,28], note: ""
+                {
+            year: "2026",month: "4",cal_1: "cal-土",cal_2: "cal-2", arrDates: [11,25], note: ""
         },
-
-
-
 
 
         // data for Kyoto Asahi
         // now this is Gocomachi 
         {
-            year: "2026",month: "2",cal_1: "cal-3土",cal_2: "cal-1", arrDates: [21], note: ""
+            year: "2026",month: "3",cal_1: "cal-3土",cal_2: "cal-1", arrDates: [21], note: ""
         },
-        {
-            year: "2026",month: "3",cal_1: "cal-3土",cal_2: "cal-2", arrDates: [21], note: ""
+                {
+            year: "2026",month: "4",cal_1: "cal-3土",cal_2: "cal-2", arrDates: [18], note: ""
         },
+
+        
 
 
         // data for Kyoto JEUGIA
         {
-            year: "2026",month: "2",cal_1: "cal-2金",cal_2: "cal-1", arrDates: [27], note: ""
+            year: "2026",month: "3",cal_1: "cal-2金",cal_2: "cal-1", arrDates: [27], note: ""
         },
         {
-            year: "2026",month: "3",cal_1: "cal-2金",cal_2: "cal-2", arrDates: [27], note: ""
+            year: "2026",month: "4",cal_1: "cal-2金",cal_2: "cal-2", arrDates: [24], note: ""
         },
 
 
         
         // data for Fukui Wednesday month classes
         {
-            year: "2026",month: "2",cal_1: "cal-水",cal_2: "cal-1", arrDates: [18], note: ""
+            year: "2026",month: "3",cal_1: "cal-水",cal_2: "cal-1", arrDates: [4,18], note: ""
         },
-        {
-            year: "2026",month: "3",cal_1: "cal-水",cal_2: "cal-2", arrDates: [4,18], note: ""
+                {
+            year: "2026",month: "4",cal_1: "cal-水",cal_2: "cal-2", arrDates: [8,22], note: ""
         },
 
 
         // data for Fukui Newspaper classes
         {
-            year: "2026",month: "2",cal_1: "cal-火",cal_2: "cal-1", arrDates: [17], note: ""
+            year: "2026",month: "3",cal_1: "cal-火",cal_2: "cal-1", arrDates: [3,17], note: ""
         },
         {
-            year: "2026",month: "3",cal_1: "cal-火",cal_2: "cal-2", arrDates: [3,17], note: ""
+            year: "2026",month: "4",cal_1: "cal-火",cal_2: "cal-2", arrDates: [7,21], note: ""
         },
 
 
@@ -73,10 +72,10 @@
 
         // data for Fukui Sabae classes
         {
-            year: "2026",month: "2",cal_1: "cal-月",cal_2: "cal-1", arrDates: [16], note: ""
+            year: "2026",month: "3",cal_1: "cal-月",cal_2: "cal-1", arrDates: [2,16], note: ""
         },
-                {
-            year: "2026",month: "3",cal_1: "cal-月",cal_2: "cal-2", arrDates: [2,16], note: ""
+        {
+            year: "2026",month: "4",cal_1: "cal-月",cal_2: "cal-2", arrDates: [6,20], note: ""
         },
 
 
