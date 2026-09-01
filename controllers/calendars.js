f197ad5e68ec053f292cc19fb1d5f0edb17aa507
Steve Duncan <Steve-Duncan@users.noreply.github.com>
2026-08-31 17:22:05 -0700

    Remove note from class for October.

--- controllers/calendars.js
+++ controllers/calendars.js
@@ -21,7 +21,7 @@
             year: "2026",month: "9",cal_1: "cal-土",cal_2: "cal-1", arrDates: [12,26], note: "＊26日ウィングス京都"
         },
         {
-            year: "2026",month: "10",cal_1: "cal-土",cal_2: "cal-2", arrDates: [17,31], note: "＊26日ウィングス京都"
+            year: "2026",month: "10",cal_1: "cal-土",cal_2: "cal-2", arrDates: [17,31], note: ""
         },
 
 
